e013710c6c0cf97a3d4c15a6a5f329f2beb7c6ed
Axel-Wolkowicz <wolkowiczaxel@gmail.com>
2026-05-21 11:55:30 -0300

    Implemented score manager

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
 {
+    public UIMANAGER UIMGR;
+    public int score = 0;
+
+
+    void Awake(){
+        UIMGR = GameObject.FindObjectOfType<UIMANAGER>();
+    }
+
    void OnTriggerEnter(Collider other){
     if(other.gameObject.CompareTag("Collectible")){
         Destroy(other.gameObject);
+        score++;
+        UIMGR.UpdateScore(score);
     }
 
     Debug.Log(other.gameObject.name);
---
+++ Assets/Scripts/UIMANAGER.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIMANAGER : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateScore(int score){
+        txtScore.text = "score: " + score.ToString();
+    }
+}
